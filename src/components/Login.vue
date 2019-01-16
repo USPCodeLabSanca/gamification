87c0efefa1bf418930b5394fc24f60c8e8230923
arenasoy <caro.arenas2607@gmail.com>
2019-01-16 16:27:32 -0200

    login with facebook

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,31 @@
-  <template>
-    <div class="col-xs-4 col-md-4">
-      <h2>Login</h2>
-      <div class="form-group">
-        <input
-          type="text"
-          class="form-control"
-          placeholder="usuario"
-          v-model="credentials.username"
-        >
-      </div>
-      <div class="form-group">
-        <input
-          type="password"
-          class="form-control"
-          placeholder="senha"
-          v-model="credentials.password"
-        >
-      </div>
-      <button class="btn btn-primary login" @click="submit()">Entrar</button>
+<template>
+  <div class="col-xs-4 col-md-4">
+    <h2>Login</h2>
+    <div class="form-group">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="usuario"
+        v-model="credentials.username"
+      >
     </div>
-  </template>
+    <div class="form-group">
+      <input
+        type="password"
+        class="form-control"
+        placeholder="senha"
+        v-model="credentials.password"
+      >
+    </div>
+    <button class="btn btn-primary login" @click="submit()">Entrar</button>
+    <facebook-login class="button"
+      appId="255459355343445"
+      @login="onLogin"
+      @logout="onLogout"
+      @sdk-loaded="sdkLoaded">
+    </facebook-login>
+  </div>
+</template>
 
 <style scoped>
 h2 {
@@ -33,6 +39,7 @@ button {
 </style>
 
 <script>
+import facebookLogin from 'facebook-login-vuejs';
 
 export default {
     data() {
@@ -46,7 +53,32 @@ export default {
     methods: {
         submit() {
             console.log('user: ' + this.credentials.username + ' - pass: ' + this.credentials.password);
+        },
+        getUserData() {
+          this.FB.api('/me', 'GET', { fields: 'id, name, email' },
+            userInformation => {
+              console.warn("data api", userInformation)
+              this.personalID = userInformation.id;
+              this.email = userInformation.email;
+              this.name = userInformation.name;
+            }
+          )
+        },
+        sdkLoaded(payload) {
+          this.isConnected = payload.isConnected
+          this.FB = payload.FB
+          if (this.isConnected) this.getUserData()
+        },
+        onLogin() {
+          this.isConnected = true
+          this.getUserData()
+        },
+        onLogout() {
+          this.isConnected = false;
         }
+      },
+    components: {
+      facebookLogin
     }
 }
 
