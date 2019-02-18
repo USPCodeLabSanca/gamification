de2262f75559409ef484d7b4929a02c39ae4bf56
arenasoy <caro.arenas2607@gmail.com>
2019-02-18 00:15:50 -0300

    Add links to credits images

--- src/views/Credits.vue
+++ src/views/Credits.vue
@@ -1,9 +1,13 @@
 <template>
   <q-page padding class="docs-input justify-center">
     <p>A gamificação da semana de recepção foi desenvolvida por</p>
+    <a href="https://www.fog.icmc.usp.br/"> 
     <img src="../assets/fog.jpg" class="responsive" height="60%" width="60%">
+    </a>
     <p>O aplicativo BixoQuest foi desenvolvido por</p>
+    <a href="https://www.facebook.com/uspcodelabsanca/">
     <img src="../assets/codelab.png" class="responsive">
+    </a>
   </q-page>
 </template>
 
